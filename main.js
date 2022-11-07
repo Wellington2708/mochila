cd3fef669b44c2d5fdc499f66da541d201e9b234
Wellington2708 <wellingtondevelopercontact@gmail.com>
2022-11-07 21:37:13 +0000

    adicionando métodos para subscrever local store

--- main.js
+++ main.js
@@ -13,16 +13,28 @@ form.addEventListener("submit" , (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+
     const itemAtual = {
         "nome" : nome.value,
         "quantidade" : quantidade.value
-}
+    }
 
-    criaElemento(itemAtual)
+    if(existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
 
-    localStorage.setItem("itens" ,JSON.stringify(itens));
+    } else {
+        itemAtual.id = itens.length
+        
+        criaElemento(itemAtual)
+        
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens" ,JSON.stringify(itens))
 
     nome.value = "" ;
     quantidade.value = "" ;
@@ -36,6 +48,7 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome    
@@ -43,4 +56,9 @@ function criaElemento(item) {
     lista.appendChild(novoItem)
 
     
+}
+
+
+function atualizaElemento(item){
+    console.log(document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade);
 }
